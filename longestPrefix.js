6e1f131a40ae6d26d83a6edb9134e770e0e41cb4
Davis Nguyen <davisnguyen@Daviss-MacBook-Air.local>
2022-09-16 15:39:57 -0500

    merged sorted linked list solved

--- longestPrefix.js
+++ longestPrefix.js
@@ -7,7 +7,7 @@ var longestCommonPrefix = function (strs) {
   for (let i = 0; i < strs[0].length; i++) {
     const char = strs[0][i]
 
-    for (let j = 0; j < strs.length; j++) {
+    for (let j = 1; j < strs.length; j++) {
       if (strs[j][i] !== char) return common
     }
 
